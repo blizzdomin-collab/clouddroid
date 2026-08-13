1a2f2a6e4cbf6f5b66f6de7493bf7811cff924d2
Dominik Zachara <blizzdomin@gmail.com>
2026-08-13 22:21:55 +0200

    Fix deploy script to use npm install instead of npm ci

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -15,7 +15,7 @@ git fetch origin
 git reset --hard "origin/$BRANCH"
 
 echo "[2/5] Installing dependencies..."
-npm ci --production=false
+npm install --production=false
 
 echo "[3/5] Building..."
 npm run build
