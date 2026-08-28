0ad541d0fbb51eda8c470829827d6a2a08b4ea83
Dominik Zachara <blizzdomin@gmail.com>
2026-08-28 08:59:02 +0200

    aaaaaaasdsd

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -100,7 +100,7 @@ export const POST: APIRoute = async ({ request, clientAddress }) => {
       const response = await fetch(`${baseUrl}/v1/checkouts`, {
         method: 'POST',
         headers: {
-          'Authorization': `Bearer ${paynowApiKey}`,
+          'Authorization': `apikey ${paynowApiKey}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
